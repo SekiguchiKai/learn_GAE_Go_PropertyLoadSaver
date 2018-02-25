47e8f6afaad222fbf6a12611f6a3eae04521a01b
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-02-25 23:43:03 +0900

    APIが操作するようにした

--- modules/default/mian.go
+++ modules/default/mian.go
@@ -1,5 +1,24 @@
 package main
 
-func init() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/SekiguchiKai/learn_GAE_Go_PropertyLoadSaver/api"
+	"net/http"
+)
 
+const _APIPath = "/api"
+
+func init() {
+	g := gin.New()
+	initAPI(g)
+	// gin.New()の戻り値のEngineは、ServeHTTP(ResponseWriter, *Request)メソッドを持っているので、
+	// type Handler interfaceを満た
+	http.Handle("/", g)
+}
+
+
+
+func initAPI(g *gin.Engine){
+	apiGin := g.Group(_APIPath)
+	api.InitUserAPI(apiGin)
 }
